a7e55f34970bbd2905753f53d504c0fd62e23dd4
Batin EVIRGEN <batinevirgen@gmail.com>
2022-08-20 17:13:25 +0300

    fix: update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const QuillBot = require('./quillbot');
+const { splitText, readFile, writeFile } = require('./text');
 
 (async () => {
+  const sentences = splitText(await readFile('text.txt'));
+
   const bot = new QuillBot();
   await bot.createNewSession();
-  await bot.paraphrase("I'm a bot");
+
+  const results = await bot.paraphraseMultiple(sentences);
+  await writeFile('output.txt', results);
+
   await bot.closeBrowser();
 })();
