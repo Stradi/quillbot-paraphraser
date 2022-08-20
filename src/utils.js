a7f73f832e9aa39fb09faee077d0ba57a96f3272
Batin EVIRGEN <batinevirgen@gmail.com>
2022-08-20 16:19:24 +0300

    docs: add JSDoc for getWordCount in utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,8 @@
-const clipboardy = require('clipboardy');
-
+/**
+ * Return word count of a string
+ * @param {string} str
+ * @returns {number} Number of words in the string.
+ */
 const getWordCount = (str) => {
   return str.split(' ').length;
 };
