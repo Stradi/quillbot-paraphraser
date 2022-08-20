55bcbc6357796a9a30bc73418391b8799c99c097
Batin EVIRGEN <batinevirgen@gmail.com>
2022-08-20 17:12:56 +0300

    feat: add utility functions for seperating text to sentences

---
+++ src/text.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const fs = require('fs-extra');
+
+const { WORD_LIMIT } = require('./config');
+const logger = require('./logger');
+/**
+ * Splits a string into an array of sentences.
+ * @param {string} text
+ * @returns {string[]} An array of sentences.
+ */
+const splitText = (text) => {
+  logger.info('Splitting text into sentences...');
+  const cleanedText = text.replaceAll('\n', '').replaceAll('  ', '');
+  const allSentences = cleanedText
+    .substring(0, cleanedText.length - 1)
+    .split('. ');
+
+  const sentences = [];
+  let tempSentence = '';
+  for (let s of allSentences) {
+    const totalWordCount = tempSentence.split(' ').length + s.split(' ').length;
+    if (totalWordCount > WORD_LIMIT) {
+      sentences.push(tempSentence);
+      tempSentence = s + '[DOT]';
+    } else {
+      tempSentence += s + '[DOT]';
+    }
+  }
+
+  sentences.push(tempSentence);
+
+  logger.info(`Found ${sentences.length} sentences.`);
+  return sentences.map((s) => s.replaceAll('[DOT]', '.'));
+};
+
+/**
+ * Reads a file and returns the contents as a string.
+ * @param {string} filename
+ * @returns {string} The contents of the file.
+ */
+const readFile = async (filename) => {
+  logger.debug(`Reading file ${filename}...`);
+  const inputPath = path.resolve(process.cwd(), 'inputs');
+  await fs.ensureDir(inputPath);
+
+  const filePath = path.resolve(inputPath, filename);
+
+  const fileExists = await fs.pathExists(filePath);
+  if (!fileExists) {
+    return Promise.reject(
+      new Error(`File ${filename} does not exist in inputs directory.`)
+    );
+  }
+
+  const text = await fs.readFile(filePath, 'utf8');
+  return text;
+};
+
+/**
+ * Writes a file to the output directory.
+ * @param {string} filename The name of the file.
+ * @param {string[]} content The sentences to write to the file.
+ */
+const writeFile = async (filename, content) => {
+  logger.info(`Writing file ${filename}...`);
+  const outputPath = path.resolve(process.cwd(), 'outputs');
+  await fs.ensureDir(outputPath);
+
+  const filePath = path.resolve(outputPath, filename);
+  await fs.ensureFile(filePath);
+
+  await fs.writeFile(filePath, content.join('\n\n'));
+  logger.info(`Wrote file ${filename} to outputs directory.`);
+};
+
+module.exports = {
+  splitText,
+  readFile,
+  writeFile,
+};
